e4c172d4a370af2b91cc8b9a1d9fefb8fd2a2d97
luciana-t <35690858+luciana-t@users.noreply.github.com>
2018-11-04 22:17:12 -0200

    Add files via upload

--- mips8.asm
+++ mips8.asm
@@ -1,11 +1,11 @@
 #8. Dados 2 vetores A e B, positivos maiores que zero. Zero marca o final dos vetores. Calcular C[i] = a[i] - b[i];
 
-li $t0, 0
-li $t3, 0
-li $t4, 1
+li $t0, 0 #valor 0
+li $t3, 0 #auxiliar de soma
+li $t4, 1 #inicializa com 1, e logo é substituido
 loop: beq $t4, $t0, fim
-	lw $t4, 0($gp)
-	lw $t5, 24($gp)
+	lw $t4, 0($gp) #A[]
+	lw $t5, 24($gp) #B[]
 	sub $t3, $t4, $t5
 	sw $t3, 0($gp)
 	addi $gp, $gp, 4
